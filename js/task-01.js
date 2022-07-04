2b372dd486739ad60deca889e173b3a1617bf57a
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-04 21:32:11 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+//1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+
+const categoryEl = document.querySelectorAll('.item');
+const numberCategoryEl = categoryEl.length;
+console.log('Number of categories:', numberCategoryEl);
+
+// // 2. Для кожного элемента li.item у спику ul#categories, 
+// знайде і виведе в консоль текст заголовку елемента(тегу < h2 >) 
+// і кількість елементів в категорії(усіх < li >, вкладених в нього).
+const headsOfCategories = document.querySelectorAll('h2');
+const headOfCategory = headsOfCategories.textContent;
+console.log('Elements:', headOfCategory);
