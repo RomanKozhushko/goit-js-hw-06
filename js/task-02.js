6fbc69c8278ee94f4746eb05c07f030a30464d3d
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-05 15:13:35 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = ingredients.map(option => {// 1/ Створюємо li
+  const itemOfFood = document.createElement('li');
+  // 2/ Додаємо клас у li
+  itemOfFood.classList.add('item');
+  // 3/ Створюємо h2
+  const itemName = document.createElement('h2');
+  itemName.textContent = option;
+  // 4/ Додаємо h2 b li
+  itemOfFood.appendChild(itemName);
+  // 5/ Додаємо  li b ul
+  const ingredientsContainer = document.querySelector('ul')
+  ingredientsContainer.appendChild(itemOfFood);
+  console.log(itemOfFood);
+})
+
+  
+  
+
+// const option = ingredients[0];
+
+
+
+
+  
+
+
+
+//  ingredients.forEach (ingredient, index){}
