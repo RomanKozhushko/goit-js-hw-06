288be4f635e33dc52fda92b38bae42363926e634
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-05 12:42:07 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -7,9 +7,11 @@ console.log('Number of categories:', numberCategoryEl);
 // // 2. Для кожного элемента li.item у спику ul#categories, 
 // знайде і виведе в консоль текст заголовку елемента(тегу < h2 >) 
 // і кількість елементів в категорії(усіх < li >, вкладених в нього).
-categoryEl.forEach(function (El, index) {
-    
-  console.log(`Індекс ${index + 1}, значення ${El}`);
+[...categoryEl].forEach(function (el, index) {
+    const numberOfElements = el.querySelectorAll('li');
+    const nameOfElements = el.querySelector('h2');
+    console.log(`Category: ${nameOfElements.textContent}`);
+    console.log(`Elements: ${numberOfElements.length}`);
 });
 
 // const numberInCategory = numberInCategories.length;
