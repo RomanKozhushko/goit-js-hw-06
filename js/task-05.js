5355a15161740f388c4e4986723a67c08d434574
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 16:50:22 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -7,7 +7,7 @@
 
 
 
-<p><a href="index.html">Go back</a></p>
+{/* <p><a href="index.html">Go back</a></p>
 
     <input type="text" id="name-input" placeholder="Please enter your name" />
-    <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+    <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */}
