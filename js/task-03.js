4abadc9ab421b036f8f228ec84b487be9af3aeee
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 10:22:30 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,34 +1,29 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
+// const images = [
+//   {
+//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
 
-const elements = images.map(option => {
-  // 1/ Створюємо li
-  const itemOfimg = document.createElement('li');
-  // 2/ Створюємо img
-  const imageEl = document.createElement('img')
-  imageEl.src = option.url;
-  imageEl.alt = option.alt;
-  // 3/ Додаємо img b li
-  itemOfimg.appendChild(imageEl);
-  // // 4/ Шукаємо ul
-  const imagesContainer = document.querySelector('.gallery')
-  // //  5/ Додаємо  li b ul
-  imagesContainer.appendChild(itemOfimg);
+const elements =
+  `<li><img src = https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; alt = 'White and Black Long Fur Cat';></img></li>
+  <li><img src =  'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; alt = 'Orange and White Koi Fish Near Yellow Koi Fish';></img></li>
+  <li><img src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260; alt = 'Group of Horses Running';></img></li>`;
+ 
+const imagesContainer = document.querySelector('.gallery')
+
+imagesContainer.insertAdjacentHTML("beforeEnd", elements);
   
-  console.log(itemOfimg);
-})
+
+
 
 // 
 // imagesContainer.insertAdjacentHTML(beforeEnd, itemOfimg);
@@ -43,8 +38,7 @@ const elements = images.map(option => {
 //   // 3/ Додаємо img b li
 //   itemOfimg.appendChild(imageEl);
 //   // // 4/ Шукаємо ul
-//   const imagesContainer = document.querySelector('.gallery')
-//   // //  5/ Додаємо  li b ul
+//   const imagesContainer = document.querySelector('.gallery') // //  5/ Додаємо  li b ul
 //   imagesContainer.appendChild(itemOfimg);
   
 //   console.log(itemOfimg);
