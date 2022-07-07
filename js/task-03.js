67a93acd83914ff40eba0570d83b60563d988fa3
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 09:23:40 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -13,7 +13,6 @@ const images = [
   },
 ];
 
-
 const elements = images.map(option => {
   // 1/ Створюємо li
   const itemOfimg = document.createElement('li');
@@ -27,7 +26,26 @@ const elements = images.map(option => {
   const imagesContainer = document.querySelector('.gallery')
   // //  5/ Додаємо  li b ul
   imagesContainer.appendChild(itemOfimg);
+  
   console.log(itemOfimg);
 })
 
+// 
+// imagesContainer.insertAdjacentHTML(beforeEnd, itemOfimg);
 
+// const elements = images.map(option => {
+//   // 1/ Створюємо li
+//   const itemOfimg = document.createElement('li');
+//   // 2/ Створюємо img
+//   const imageEl = document.createElement('img')
+//   imageEl.src = option.url;
+//   imageEl.alt = option.alt;
+//   // 3/ Додаємо img b li
+//   itemOfimg.appendChild(imageEl);
+//   // // 4/ Шукаємо ul
+//   const imagesContainer = document.querySelector('.gallery')
+//   // //  5/ Додаємо  li b ul
+//   imagesContainer.appendChild(itemOfimg);
+  
+//   console.log(itemOfimg);
+// })
