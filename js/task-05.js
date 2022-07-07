6eff3bd7add48d129852a1a6926eb8f3323f93e2
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 17:13:09 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,6 @@
+const input = document.querySelector('#name-input');
+const span = document.querySelector('#name-output > span')
+const span = document.querySelector('#name-output')
 
 
 
@@ -5,9 +8,7 @@
 
 
 
+// {/* <p><a href="index.html">Go back</a></p>
 
-
-{/* <p><a href="index.html">Go back</a></p>
-
-    <input type="text" id="name-input" placeholder="Please enter your name" />
-    <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */}
+//     <input type="text" id="name-input" placeholder="Please enter your name" />
+//     <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */}
