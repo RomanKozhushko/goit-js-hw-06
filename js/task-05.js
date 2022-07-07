7c18f57eb32c9c45c849ff2c9ab8256b3fc9374a
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 19:59:36 +0300

    +

--- js/task-05.js
+++ js/task-05.js
@@ -12,8 +12,3 @@ function onInputChange (event) {
 
 
 
-
-// {/* <p><a href="index.html">Go back</a></p>
-
-//     <input type="text" id="name-input" placeholder="Please enter your name" />
-//     <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */}
---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+//  <p><a href="index.html">Go back</a></p>
+
+//     <input
+//       type="text"
+//       id="validation-input"
+//       data-length="6"
+//       placeholder="Please enter 6 symbols"
+//     />
