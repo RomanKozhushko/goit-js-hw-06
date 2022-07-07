bb09515a55820496d4c90a89803a29f2fcd9cc61
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 16:48:38 +0300

    +

--- js/task-03.js
+++ js/task-03.js
@@ -1,32 +1,28 @@
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-
-const elements =
-  `<li><img src = https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; alt = 'White and Black Long Fur Cat';></img></li>
-  <li><img src =  'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'; alt = 'Orange and White Koi Fish Near Yellow Koi Fish';></img></li>
-  <li><img src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260; alt = 'Group of Horses Running';></img></li>`;
- 
-const imagesContainer = document.querySelector('.gallery')
-
-imagesContainer.insertAdjacentHTML("beforeEnd", elements);
-  
-
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+// Створюємо шаблонний рядочок!
+const galleryOfImg = ({ url, alt }) => `<li><img src="${url}" alt="${alt}"></li>`;
+// Створюємо акумулятор
+const galleryMarkup = images.reduce((acc, img) => acc + galleryOfImg(img), "");
+//Шукаємо  class = gallery
+const galleryList = document.querySelector(".gallery");
+//Додаємо шаблоні рядки
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryList.setAttribute("style", "list-style-type:none; display: flex;");
 
 
-// 
-// imagesContainer.insertAdjacentHTML(beforeEnd, itemOfimg);
 
 // const elements = images.map(option => {
 //   // 1/ Створюємо li
