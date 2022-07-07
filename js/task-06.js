42cb031ffe4983ad0deaad43a5a7460bdffca283
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-07 23:15:32 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,23 @@
 
+//1/шукаємо колір рамки інпуту через ЛЕТ томущо значення буде змінюватись через наявність трьох кольорів!
+let input = document.querySelector("#validation-input");
+//2/витягуємо значення довжини інпута
+const inputLength = input.getAttribute("data-length");
+
+
+input.oninput = function() {
+    if(input.value.length > inputLength) {
+        input.classList.remove("invalid");
+        input.classList.add("valid");
+    }
+    if( input.value.length < inputLength ) {
+        input.classList.add("invalid");
+        input.classList.remove("valid");
+    }
+    if( input.value.length === 0) {
+        input.classList.toggle("invalid")
+    }
+}
 
 
 
@@ -6,11 +25,6 @@
 
 
 
-//  <p><a href="index.html">Go back</a></p>
 
-//     <input
-//       type="text"
-//       id="validation-input"
-//       data-length="6"
-//       placeholder="Please enter 6 symbols"
-//     />
+
+
