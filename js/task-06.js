dc63319ce000f4650772957132c7908e33b1e325
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-08 11:17:46 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -20,7 +20,15 @@ input.oninput = function () {
     }
 }
 
-
+     #validation-input {
+        border: 2px solid #bdbdbd;
+      }
+      #validation-input.valid {
+        border-color: #4caf50;
+      }
+      #validation-input.invalid {
+        border-color: #f44336;
+      }
 
 
 
