9c1938cbca6d5740ea775beb17fc1f5d43fa3288
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-08 12:41:30 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+(function () {
+    const range = document.querySelector('#font-size-control');
+    const text = document.querySelector('#text');  
+    range.addEventListener("input", 
+      function() {            
+          text.style.fontSize = range.value + 'px'   
+    });
+})();
+
+
+
+
