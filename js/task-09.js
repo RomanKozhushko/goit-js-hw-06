2f3541ba8e92f4deb6e36d316f9e0a61e6478d9f
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-08 18:37:49 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
+const inputCangeColor = document.querySelector('.change-color');
+const toBody = document.querySelector('body');
+const changeNameSpan = document.querySelector('span.color');
+inputCangeColor.addEventListener('click', () => {
+  toBody.style.background = getRandomHexColor();
+  changeNameSpan.textContent = getRandomHexColor();
+});
