b75f60f5299b28f0f0b6e554f7df6f5cff73fc12
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-09 21:01:09 +0300

    ik

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,46 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = {
+    sub: document.querySelector("[data-create]"),
+    add: document.querySelector("[data-destroy]"),
+    inputName: document.querySelector('input'),
+}
+
+let counter;
+const handleClick = (event) => {
+  counter = parseInt(event.currentTarget.value);
+  console.log(counter);
+
+  }
+button.inputName.addEventListener("change", handleClick);
+
+
+const createDiv = (event) => {
+    for (let i = 0; i < counter; ++i) {
+    let div = document.createElement("div");   
+    document.body.appendChild(div);  
+  }
+  }
+button.sub.addEventListener("click", createDiv);
+
+
+// 
+
+
+// const elements = ingredients.map(option => {// 1/ Створюємо li
+//   const itemOfFood = document.createElement('li');
+//   // 2/ Додаємо клас у li
+//   itemOfFood.classList.add('item');
+//   // 3/ Створюємо h2
+//   const itemName = document.createElement('h2');
+//   itemName.textContent = option;
+//   // 4/ Додаємо h2 b li
+//   itemOfFood.appendChild(itemName);
+//   // 5/ Шукаємо ul
+//   const ingredientsContainer = document.querySelector('#ingredients')
+//   //  6/ Додаємо  li b ul
+//   ingredientsContainer.appendChild(itemOfFood);
+//   console.log(itemOfFood);
+// })
