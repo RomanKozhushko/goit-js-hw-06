166c77c46c6efc81676cb02d177efabaeeb42950
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-10 10:43:53 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,25 +7,38 @@ const ingredients = [
   'Condiments',
 ];
 
-const elements = ingredients.map(option => {
-  // 1/ Створюємо li
-  const itemOfFood = document.createElement('li');
-  // 2/ Додаємо клас у li
+ const elements = ingredients.map(option => {
+ const itemOfFood = document.createElement('li');
   itemOfFood.classList.add('item');
-  // 3/ Створюємо h2
   const itemName = document.createElement('h2');
   itemName.textContent = option;
-  // 4/ Додаємо h2 b li
   itemOfFood.appendChild(itemName);
-  // 5/ Шукаємо ul
-  const ingredientsContainer = document.querySelector('#ingredients')
-  //  6/ Додаємо  li b ul
-  ingredientsContainer.append(itemOfFood);
-  console.log(itemOfFood);
-})
+   
+ }) 
+ const ingredientsContainer = document.querySelector('#ingredients');
+ingredientsContainer.append(...elements);
 
+
+
+
+// const elements = ingredients.map(option => {
+//   // 1/ Створюємо li
+//   const itemOfFood = document.createElement('li');
+//   // 2/ Додаємо клас у li
+//   itemOfFood.classList.add('item');
+//   // 3/ Створюємо h2
+//   const itemName = document.createElement('h2');
+//   itemName.textContent = option;
+//   // 4/ Додаємо h2 b li
+//   itemOfFood.appendChild(itemName);
+//   // 5/ Шукаємо ul
+   
+// })
+//  const ingredientsContainer = document.querySelector('#ingredients')
+//  ingredientsContainer.append(...elements);
   
   
+
 
 
 
