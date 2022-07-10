26ce1001dc8495763972bc7405fe053f68bb3d17
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-10 11:26:51 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -12,34 +12,25 @@ const ingredients = [
   itemOfFood.classList.add('item');
   const itemName = document.createElement('h2');
   itemName.textContent = option;
-  itemOfFood.appendChild(itemName);
-   
- }) 
- const ingredientsContainer = document.querySelector('#ingredients');
-ingredientsContainer.append(...elements);
+   itemOfFood.appendChild(itemName);
+   return itemOfFood;
+  }) 
+  const ingredientsContainer = document.querySelector('#ingredients'); 
+   ingredientsContainer.append(... elements);
 
 
 
 
-// const elements = ingredients.map(option => {
-//   // 1/ Створюємо li
-//   const itemOfFood = document.createElement('li');
-//   // 2/ Додаємо клас у li
-//   itemOfFood.classList.add('item');
-//   // 3/ Створюємо h2
-//   const itemName = document.createElement('h2');
-//   itemName.textContent = option;
-//   // 4/ Додаємо h2 b li
-//   itemOfFood.appendChild(itemName);
-//   // 5/ Шукаємо ul
-   
+// const elementUl = document.querySelector("#ingredients")
+// // console.log(elementUl)
+// const elementIngredients = ingredients.map(ingredient => {
+//   const elementList = document.createElement("li");
+//   elementList.textContent = ingredient;
+
+//   return elementList;
 // })
-//  const ingredientsContainer = document.querySelector('#ingredients')
-//  ingredientsContainer.append(...elements);
-  
-  
-
-
-
+// // console.log(elIngredients)
+// elementUl.append(...elementIngredients)
+// //console.log(elementUl)
 
 
