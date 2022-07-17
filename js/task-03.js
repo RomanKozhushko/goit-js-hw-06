6c26c841f95b6e731e29b0dc8bd9ee5795f2b603
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-17 12:06:46 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -22,20 +22,3 @@ const galleryList = document.querySelector(".gallery");
 galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
 galleryList.setAttribute("style", "list-style-type:none; display: flex;");
 
-
-
-// const elements = images.map(option => {
-//   // 1/ Створюємо li
-//   const itemOfimg = document.createElement('li');
-//   // 2/ Створюємо img
-//   const imageEl = document.createElement('img')
-//   imageEl.src = option.url;
-//   imageEl.alt = option.alt;
-//   // 3/ Додаємо img b li
-//   itemOfimg.appendChild(imageEl);
-//   // // 4/ Шукаємо ul
-//   const imagesContainer = document.querySelector('.gallery') // //  5/ Додаємо  li b ul
-//   imagesContainer.appendChild(itemOfimg);
-  
-//   console.log(itemOfimg);
-// })
