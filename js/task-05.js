050d9cdea48c5e7ad1755a44d759fe9d56a0fa82
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2023-01-21 19:58:08 +0000

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,16 @@
+//Достукуємось до поля input через id #name-input, та зчитуэмо введений текст
 const inputName = document.querySelector('#name-input');
+
+// Достукуємось до <span> куди будемо вписувати текст з поля input
 const changeNameSpan = document.querySelector('#name-output');
-const button = document.querySelector('#name-output');
+
+// Функція перенесення тексту з input в Span
+// Ця функція додає обробник події 'input' => для елементу inputName.
+// Коли користувач вводить текст у це поле, функція перевіряє значення поля
+// і в залежності від цього змінює значення тексту у елементі changeNameSpan.
+// Якщо значення поля не порожнє, то значення тексту в changeNameSpan встановлюється
+// до введеного тексту без пробілів. Якщо ж значення поля є порожнім,
+// то значення тексту в changeNameSpan встановлюється до 'Anonymous'!!!
 
 inputName.addEventListener('input', () => {
     if (inputName.value !== '') {
